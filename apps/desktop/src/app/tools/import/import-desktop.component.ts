e858b18d8e31bebdac609e5a8b16c7e8075d67c0
Daniel James Smith <djsmith85@users.noreply.github.com>
2023-10-04 23:48:22 +0200

    Enable importing on deskop
    Create import-dialog
    Create file-menu entry to open import-dialog
    Extend messages.json to include all the necessary messages from shared components

---
+++ apps/desktop/src/app/tools/import/import-desktop.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { AsyncActionsModule, ButtonModule, DialogModule } from "@bitwarden/components";
+import { ImportComponent } from "@bitwarden/importer";
+
+@Component({
+  selector: "app-import-desktop",
+  templateUrl: "import-desktop.component.html",
+  standalone: true,
+  imports: [
+    CommonModule,
+    JslibModule,
+    DialogModule,
+    AsyncActionsModule,
+    ButtonModule,
+    ImportComponent,
+  ],
+})
+export class ImportDesktopComponent {}
