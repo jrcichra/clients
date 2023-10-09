1f7533bbba44bd33aa42f67c3ec80efad5debc32
Daniel James Smith <djsmith85@users.noreply.github.com>
2023-10-09 18:13:40 +0200

    Make ImportWebComponent standalone, simplify routing

--- apps/web/src/app/tools/import/import-web.component.ts
+++ apps/web/src/app/tools/import/import-web.component.ts
@@ -1,7 +1,24 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+
+import { JslibModule } from "@bitwarden/angular/jslib.module";
+import { AsyncActionsModule, ButtonModule } from "@bitwarden/components";
+import { ImportComponent } from "@bitwarden/importer";
 
 @Component({
   selector: "app-import-web",
   templateUrl: "import-web.component.html",
+  standalone: true,
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    RouterModule,
+    JslibModule,
+    AsyncActionsModule,
+    ButtonModule,
+    ImportComponent,
+  ],
 })
 export class ImportWebComponent {}
