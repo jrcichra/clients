6e7554e9880809b2555aa59c81ebad44f0a44c7a
Daniel James Smith <djsmith85@users.noreply.github.com>
2023-10-10 23:11:59 +0200

    Fix import path for ImportComponent

--- apps/browser/src/tools/popup/settings/import/import-browser.component.ts
+++ apps/browser/src/tools/popup/settings/import/import-browser.component.ts
@@ -4,7 +4,7 @@ import { Router, RouterLink } from "@angular/router";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { AsyncActionsModule, ButtonModule, DialogModule } from "@bitwarden/components";
-import { ImportComponent } from "@bitwarden/importer";
+import { ImportComponent } from "@bitwarden/importer/ui";
 
 @Component({
   templateUrl: "import-browser.component.html",
