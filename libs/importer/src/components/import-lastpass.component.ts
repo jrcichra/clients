afda22a884107d2da0d767ca76620ff20c2f77d6
William Martin <contact@willmartian.com>
2023-10-11 15:15:50 -0400

    refactor to password dialog approach

--- libs/importer/src/components/import-lastpass.component.ts
+++ libs/importer/src/components/import-lastpass.component.ts
@@ -1,27 +1,14 @@
 import { CommonModule } from "@angular/common";
-import { Component, EventEmitter, OnDestroy, OnInit, Output } from "@angular/core";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
 import {
-  AbstractControl,
   AsyncValidatorFn,
   ControlContainer,
   FormBuilder,
   FormGroup,
   ReactiveFormsModule,
-  ValidationErrors,
   Validators,
 } from "@angular/forms";
-import {
-  Subject,
-  combineLatest,
-  debounceTime,
-  distinctUntilChanged,
-  filter,
-  firstValueFrom,
-  map,
-  share,
-  switchMap,
-  takeUntil,
-} from "rxjs";
+import { map } from "rxjs";
 
 import { JslibModule } from "@bitwarden/angular/jslib.module";
 import { TokenService } from "@bitwarden/common/auth/abstractions/token.service";
@@ -29,6 +16,7 @@ import { CryptoFunctionService } from "@bitwarden/common/platform/abstractions/c
 import {
   CalloutModule,
   CheckboxModule,
+  DialogService,
   FormFieldModule,
   IconButtonModule,
   TypographyModule,
@@ -52,11 +40,8 @@ import { Vault } from "../importers/lastpass/access/vault";
     CheckboxModule,
   ],
 })
-export class ImportLastPassComponent implements OnInit, OnDestroy {
-  private _destroy$ = new Subject<void>();
-
-  /** mask password input */
-  protected showPassword = false;
+export class ImportLastPassComponent implements OnInit {
+  private vault: Vault;
 
   private _parentFormGroup: FormGroup;
   protected formGroup = this.formBuilder.group({
@@ -64,43 +49,19 @@ export class ImportLastPassComponent implements OnInit, OnDestroy {
       "",
       {
         validators: [Validators.required, Validators.email],
-        asyncValidators: [this.validateEmailField()],
-        updateOn: "change",
-      },
-    ],
-    password: [
-      "",
-      {
-        validators: [Validators.required],
-        asyncValidators: [this.validatePasswordField()],
+        asyncValidators: [this.submit()],
         updateOn: "submit",
       },
     ],
     includeSharedFolders: [],
   });
-
-  private vault: Vault;
-
-  private password$ = this.formGroup.controls.password.valueChanges;
-
-  private email$ = this.formGroup.controls.email.valueChanges.pipe(distinctUntilChanged());
   protected emailHint$ = this.formGroup.controls.email.statusChanges.pipe(
     map((status) => {
       if (status === "PENDING") {
         return "Finding your account...";
       }
-      if (status === "VALID") {
-        return "Account found. Ready to import.";
-      }
     })
   );
-
-  private userType$ = this.email$.pipe(
-    debounceTime(4000),
-    switchMap((email) => this.getUserTypeByEmail(email)),
-    share()
-  );
-  protected isFederatedSSO$ = this.userType$.pipe(map((userType) => userType?.isFederated()));
 
   @Output() csvDataLoaded = new EventEmitter<string>();
 
@@ -108,7 +69,8 @@ export class ImportLastPassComponent implements OnInit, OnDestroy {
     tokenService: TokenService,
     cryptoFunctionService: CryptoFunctionService,
     private formBuilder: FormBuilder,
-    private controlContainer: ControlContainer
+    private controlContainer: ControlContainer,
+    private dialogService: DialogService
   ) {
     this.vault = new Vault(cryptoFunctionService, tokenService);
   }
@@ -116,84 +78,46 @@ export class ImportLastPassComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this._parentFormGroup = this.controlContainer.control as FormGroup;
     this._parentFormGroup.addControl("lastpassOptions", this.formGroup);
-
-    const _formIsValid$ = this.formGroup.statusChanges.pipe(filter((status) => status === "VALID"));
-    combineLatest([this.email$, this.password$, this.userType$, _formIsValid$])
-      .pipe(
-        switchMap(async ([email, password, userType]) =>
-          this.loadCSVData(email, password, userType)
-        ),
-        takeUntil(this._destroy$)
-      )
-      .subscribe();
   }
 
-  ngOnDestroy(): void {
-    this._destroy$.next();
-    this._destroy$.complete();
-  }
+  private submit(): AsyncValidatorFn {
+    return async () => {
+      const email = this.formGroup.value.email;
 
-  private async loadCSVData(email: string, password: string, userType: any) {
-    /** MP */
-    if (userType.type === 0) {
-      // this.vault.open(email, password)
-    }
+      try {
+        await this.vault.setUserTypeContext(email);
+      } catch {
+        return {
+          accountNotFound: {
+            message: "Cannot retrieve account",
+          },
+        };
+      }
 
-    /** Federated */
-    if (userType.type === 3) {
-      // this.vault.openFederated()
-    }
+      if (this.vault.userType.isFederated()) {
+        // TODO placeholder dialog
+        await this.dialogService.openSimpleDialog({
+          title: "Enter 2fa",
+          type: "info",
+          content: "Please enter your 2fa",
+        });
 
-    const csvData = `dummy data ${email} ${password} ${JSON.stringify(userType)}`;
-    // eslint-disable-next-line no-console
-    console.log(csvData);
-    this.csvDataLoaded.emit(csvData);
-  }
+        // await this.vault.openFederated()
+      } else {
+        // TODO placeholder dialog
+        await this.dialogService.openSimpleDialog({
+          title: "Enter password",
+          type: "info",
+          content: "Please enter your password",
+        });
 
-  /**
-   * @param email Email of the account to find
-   * @returns Returns the UserType if it exists, otherwise returns `null`
-   */
-  private async getUserTypeByEmail(email: string) {
-    try {
-      await this.vault.setUserTypeContext(email);
-      return this.vault.userType;
-    } catch {
+        // await this.vault.open(email, '');
+      }
+
+      // const csvData = this.vault.accountsToExportedCsvString()
+      const csvData = `csv data ${email}`;
+      this.csvDataLoaded.emit(csvData);
       return null;
-    }
-  }
-
-  /**
-   * TODO:
-   *
-   * Give user feedback if the LP password + email combo is incorrect
-   */
-  private validatePasswordField(): AsyncValidatorFn {
-    const errors: ValidationErrors = {
-      badEmailOrPassword: {
-        message: "Incorrect email or password",
-      },
     };
-    return (_control: AbstractControl): Promise<ValidationErrors> => {
-      // return null if password is valid, else return errors. See `validateEmailField`
-      return Promise.resolve(errors);
-    };
-  }
-
-  private validateEmailField(): AsyncValidatorFn {
-    const errors: ValidationErrors = {
-      accountNotFound: {
-        message: "Cannot retrieve account",
-      },
-    };
-
-    return () =>
-      firstValueFrom(this.userType$).then((account) => {
-        if (account === null) {
-          this.formGroup.controls.email.markAsTouched();
-          return errors;
-        }
-        return null;
-      });
   }
 }
